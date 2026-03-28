905875d5b5db52c6cb97f4d3499567c4f8b62d7a
heitorsDev <heitorhilles@gmail.com>
2026-03-28 15:25:50 -0300

    projectcontroller now has all the links

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -10,8 +10,8 @@ exports.getAllProjects = async (req, res) => {
 };
 exports.createProject = async (req, res) => {
   try {
-    const { title, description, imageLink } = req.body;
-    const project = await Project.create({ title, description, imageLink });
+    const { title, description, imageLink, mainLink, githubLink, youtubeLink, onshapeLink } = req.body;
+    const project = await Project.create({ title, description, imageLink, mainLink, githubLink, youtubeLink, onshapeLink });
     res.status(201).json(project);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -20,8 +20,8 @@ exports.createProject = async (req, res) => {
 exports.editProject = async (req, res) => {
   try {
     const { id } = req.params;
-    const { title, description, imageLink } = req.body;
-    const project = await Project.update({ title, description, imageLink }, { where: { id } });
+    const { title, description, imageLink, mainLink, githubLink, youtubeLink, onshapeLink } = req.body;
+    const project = await Project.update({ title, description, imageLink, mainLink, githubLink, youtubeLink, onshapeLink }, { where: { id } });
     res.json(project);
   } catch (error) {
     res.status(500).json({ message: error.message });
