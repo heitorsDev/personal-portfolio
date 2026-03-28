6ffde8e192fb18661affb3c8a7b939338a4774e2
heitorsDev <heitorhilles@gmail.com>
2026-03-28 15:20:12 -0300

    project controller

---
+++ src/controllers/projectController.js
@@ -0,0 +1,38 @@
+const Project = require('../models/Project');
+
+exports.getAllProjects = async (req, res) => {
+  try {
+    const projects = await Project.findAll();
+    res.json(projects);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+exports.createProject = async (req, res) => {
+  try {
+    const { title, description, imageLink } = req.body;
+    const project = await Project.create({ title, description, imageLink });
+    res.status(201).json(project);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+exports.editProject = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, description, imageLink } = req.body;
+    const project = await Project.update({ title, description, imageLink }, { where: { id } });
+    res.json(project);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+exports.deleteProject = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const project = await Project.destroy({ where: { id } });
+    res.json(project);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
