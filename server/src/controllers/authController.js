e74b3a8e3b4be37371b61bdbb577f7bdf6244872
heitorsDev <heitorhilles@gmail.com>
2026-03-28 20:09:41 -0300

    solved auth to allow cookies beetwen different domains

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -28,7 +28,8 @@ exports.register = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      secure: true,
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
@@ -69,7 +70,8 @@ exports.login = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      secure: true,
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
@@ -89,7 +91,11 @@ exports.login = async (req, res) => {
 };
 
 exports.logout = (req, res) => {
-  res.clearCookie('token');
+  res.clearCookie('token', {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none',
+  });
   return res.json({ message: 'Logged out successfully' });
 };
 
